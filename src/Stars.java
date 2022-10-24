2bb06ecf10cd7522647301e8ecb8212e808288ab
YusufAmat <hoccayusuf@gmail.com>
2022-10-24 20:36:21 +0200

    odev3Stars yapildi ve düzeltildi.

--- src/Stars.java
+++ src/Stars.java
@@ -3,21 +3,7 @@ import java.util.Scanner;
 public class Stars {
     public static void main(String[] args) {
         //TODO: Klavyeden girilen sayı kadar yıldızı ekrana tablo halinde yazan bir program yazınız.
-        /*
-            Ekran:
-            Yıldız Adedi:
-            Sütun Adedi :
 
-
-            Örnek;
-            Yıldız Adedi: 54
-            Sütun Adedi : 12
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*
-         */
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Lütfen yildiz adedini giriniz : ");
